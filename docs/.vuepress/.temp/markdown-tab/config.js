1f819d7bef8e59398cb37956b67fd5b5def0ef7a
Wu KunJun <107984291+ColinHouse@users.noreply.github.com>
2025-11-13 11:46:54 +0800

    feat: Initial clean upload to new repository

---
+++ docs/.vuepress/.temp/markdown-tab/config.js
@@ -0,0 +1,10 @@
+import { CodeTabs } from "E:/my-blog/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
+import { Tabs } from "E:/my-blog/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
+import "E:/my-blog/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
+
+export default {
+  enhance: ({ app }) => {
+    app.component("CodeTabs", CodeTabs);
+    app.component("Tabs", Tabs);
+  },
+};
